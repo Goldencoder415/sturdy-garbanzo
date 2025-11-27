2bd2c887992d52ebfb12f341bb40f6d6af724b43
Goldencoder415 <hamzakhakwani2016@gmail.com>
2025-11-27 17:57:12 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+my_dict={}
+
+my_dict={1:'apple',2:'ball'}
+
+my_dict = {'name': 'hamza', 1:{2,4,3}}
+
+my_dict = {'name': 'hamza', 'age':10}
+
+print(my_dict['name'])                                              
+print(my_dict.get('age'))
+my_dict['age']=10                                      
+print(my_dict)
+
+my_dict['address']='Bahawalpur'
+print(my_dict)
